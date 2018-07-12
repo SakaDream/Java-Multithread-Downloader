2fffca0a9e2480415c06d386b550e55e0f631fe2
SakaDream <bahaimt@gmail.com>
2018-07-12 23:31:03 +0700

    Update Appication

--- src/main/java/com/sakadream/downloader/App.java
+++ src/main/java/com/sakadream/downloader/App.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,6 +20,7 @@ public class App {
                     "Please input URL, ex: java -jar target\\multithread-downloader-1.0-SNAPSHOT-jar-with-dependencies.jar https://example.com/path/file.txt");
         } else {
             DownloadFile downloadFile = DownloadFile.getInstance();
+            HttpURLConnection.setFollowRedirects(false);
             try {
                 String urlStr = Utils.getUrl(args);
                 Config config = Config.getInstance();
@@ -32,32 +32,35 @@ public class App {
                     System.exit(1);
                 }
 
-                URL url = new URL(urlStr);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-
-                long fileSize = connection.getContentLengthLong();
-                String filename = Utils.getFilename(url, connection);
-
-                downloadFile.setFilename(Utils.setFilename(config.getDownloadsLocation(), filename));
-                downloadFile.setFileSize(fileSize);
-                downloadFile.setUrl(url);
-                downloadFile.setRandomString();
+                Utils.getDownloadFileInfo(urlStr);
+                String filename = DownloadFile.getInstance().getFilename();
 
                 System.out.format("Filename: %s\n", filename);
-                System.out.format("File size: %s\n\n", Utils.humanReadableByteCount(downloadFile.getFileSize(), true));
+                System.out.format("File size: %s\n", Utils.humanReadableByteCount(downloadFile.getFileSize(), true));
+
+                boolean isPartialDownload = Utils.canPartialDownloading();
+                System.out.format("Partial Download? %s\n", isPartialDownload);
+
+                System.out.println();
 
                 List<DownloadPart> downloadParts = new ArrayList<>();
 
-                List<List<Long>> listOfStartBytesAndEndBytes = Utils
-                        .getListOfStartBytesAndEndBytes(downloadFile.getFileSize(), config.getNumberOfConnections());
+                if (isPartialDownload) {
+                    List<List<Long>> listOfStartBytesAndEndBytes = Utils.getListOfStartBytesAndEndBytes(
+                            downloadFile.getFileSize(), config.getNumberOfConnections());
 
-                for (int i = 0; i < listOfStartBytesAndEndBytes.size(); i++) {
-                    List<Long> startBytesAndEndBytes = listOfStartBytesAndEndBytes.get(i);
+                    for (int i = 0; i < listOfStartBytesAndEndBytes.size(); i++) {
+                        List<Long> startBytesAndEndBytes = listOfStartBytesAndEndBytes.get(i);
+                        DownloadPart downloadPart = new DownloadPart(
+                                Utils.getFilePartName(filename, downloadFile.getRandomString(), i),
+                                startBytesAndEndBytes.get(Constaints.START_BYTES_INDEX),
+                                startBytesAndEndBytes.get(Constaints.END_BYTES_INDEX));
+                        downloadParts.add(downloadPart);
+                    }
+                } else {
                     DownloadPart downloadPart = new DownloadPart(
-                            Utils.getFilePartName(filename, downloadFile.getRandomString(), i),
-                            startBytesAndEndBytes.get(Constaints.START_BYTES_INDEX),
-                            startBytesAndEndBytes.get(Constaints.END_BYTES_INDEX));
+                            Utils.getFilePartName(filename, downloadFile.getRandomString(), 0), 0L,
+                            downloadFile.getFileSize());
                     downloadParts.add(downloadPart);
                 }
 
@@ -67,13 +70,16 @@ public class App {
                 Downloader.getInstance().startDownload();
                 File result = Utils.mergeFiles(config.getDownloadsLocation());
 
-                System.out.format("\nDownload complete! File location: %s\n", result.getAbsolutePath());
+                System.out.println();
+
+                System.out.format("Download complete! File location: %s\n", result.getAbsolutePath());
                 Timer timer = Timer.getInstance();
                 Long downloadDuration = timer.getDownloadCompletedTime() - timer.getStartTime();
                 System.out.format("Download time: %s\n", Utils.convertDurationInMilisToString(downloadDuration));
                 System.out.format("Average download speed: %s\n",
                         Utils.humanReadableSpeed(downloadFile.getFileSize(), downloadDuration, true));
             } catch (MalformedURLException mie) {
+                mie.printStackTrace();
                 System.err.println("Invalid URL format!");
             } catch (IOException ioe) {
                 System.err.println("Error when openning connection!");
