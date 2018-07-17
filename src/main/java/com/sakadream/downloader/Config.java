0950899a9cf054b6b5cdeaaeef767adce9ed31d3
SakaDream <bahaimt@gmail.com>
2018-07-17 13:04:03 +0700

    Add proxy username, proxy password, and user agent

--- src/main/java/com/sakadream/downloader/Config.java
+++ src/main/java/com/sakadream/downloader/Config.java
@@ -1,6 +1,10 @@
 package com.sakadream.downloader;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.Objects;
+
+import org.apache.commons.lang3.SystemUtils;
 
 /**
  * Config
@@ -10,7 +14,10 @@ public class Config {
     private Integer numberOfConnections;
     private String downloadsLocation;
     private Boolean useSystemProxy;
+    private String proxyUsername;
+    private String proxyPassword;
     private Boolean enableCertificateValidation;
+    private String userAgent;
 
     private static Config instance = null;
 
@@ -67,6 +74,34 @@ public class Config {
     }
 
     /**
+     * @return the proxyUsername
+     */
+    public String getProxyUsername() {
+        return proxyUsername;
+    }
+
+    /**
+     * @param proxyUsername the proxyUsername to set
+     */
+    public void setProxyUsername(String proxyUsername) {
+        this.proxyUsername = proxyUsername;
+    }
+
+    /**
+     * @return the proxyPassword
+     */
+    public String getProxyPassword() {
+        return new String(Base64.getDecoder().decode(this.proxyPassword));
+    }
+
+    /**
+     * @param proxyPassword the proxyPassword to set
+     */
+    public void setProxyPassword(String proxyPassword) {
+        this.proxyPassword = Base64.getEncoder().encodeToString(proxyPassword.getBytes(Charset.defaultCharset()));
+    }
+
+    /**
      * @return the enableCertificateValidation
      */
     public Boolean getEnableCertificateValidation() {
@@ -78,6 +113,30 @@ public class Config {
      */
     public void setEnableCertificateValidation(Boolean enableCertificateValidation) {
         this.enableCertificateValidation = enableCertificateValidation;
+    }
+
+    /**
+     * @return the userAgent
+     */
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    /**
+     * @param userAgent the userAgent to set
+     */
+    public void setUserAgent() {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            this.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+        } else if (SystemUtils.IS_OS_LINUX) {
+            this.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+        } else if (SystemUtils.IS_OS_MAC) {
+            this.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+        } else if (SystemUtils.IS_OS_UNIX) {
+            this.userAgent = "Mozilla/5.0 (X11; U; Unix; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+        } else {
+            this.userAgent = "Mozilla/5.0 (Unknown OS) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+        }
     }
 
 }
